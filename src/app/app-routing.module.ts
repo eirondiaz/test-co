9f04beef2bb5e39d7bdc392f62285c2f6dd7d8bb
EironDiaz <eirondiaz@gmail.com>
2021-05-12 10:26:11 -0400

    routing configurado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
+import { EditarComponent } from './components/editar/editar.component';
+import { VerComponent } from './components/ver/ver.component';
+import { NuevoComponent } from './components/nuevo/nuevo.component';
+import { ListaComponent } from './components/lista/lista.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'lista',
+    component: ListaComponent
+  },
+  {
+    path: 'nuevo',
+    component: NuevoComponent
+  },
+  {
+    path: 'ver',
+    component: VerComponent
+  },
+  {
+    path: 'lista',
+    component: EditarComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/lista'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
