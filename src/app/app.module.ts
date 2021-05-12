a372aa47fb62247a616c239997ba4f5615682d91
EironDiaz <eirondiaz@gmail.com>
2021-05-12 10:16:51 -0400

    componentes y servicio creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaComponent } from './components/lista/lista.component';
+import { NuevoComponent } from './components/nuevo/nuevo.component';
+import { EditarComponent } from './components/editar/editar.component';
+import { VerComponent } from './components/ver/ver.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    NuevoComponent,
+    EditarComponent,
+    VerComponent
   ],
   imports: [
     BrowserModule,
