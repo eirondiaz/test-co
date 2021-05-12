89ef4eada76f8706da1f17339be45e6871d94aa7
EironDiaz <eirondiaz@gmail.com>
2021-05-12 10:44:10 -0400

    Servicio configurado y modelo de empleado creado

---
+++ src/app/models/Employee.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+    _id?: string
+    name?: string
+    country?: string
+    username?: string
+    hiringdate?: string
+    status?: boolean
+    area?: string
+    cargo?: string
+    comision?: number
+}
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,3 +1,5 @@
+import { Employee } from './../models/Employee';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +7,27 @@ import { Injectable } from '@angular/core';
 })
 export class DataService {
 
-  constructor() { }
+  private API: string = 'https://apirest-dr.herokuapp.com/api/v1/employees/'
+
+  constructor(private _http: HttpClient) { }
+
+  getAllEmployees() {
+    return this._http.get<any>(this.API) 
+  }
+
+  getEmployeeById(id: string) {
+    return this._http.get<any>(this.API + id) 
+  }
+
+  createEmployee(emp: Employee) {
+    return this._http.post<any>(this.API, emp) 
+  }
+
+  editEmployee(emp: Employee, id: string) {
+    return this._http.put<any>(this.API + id, emp) 
+  }
+
+  deleteEmployee(id: string) {
+    return this._http.delete<any>(this.API + id) 
+  }
 }
